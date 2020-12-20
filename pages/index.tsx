61f89879cd41515e7c703b719e16fd587b4eaf70
Wirya Ramadhan <adhaniscuber@gmail.com>
2020-12-21 00:03:59 +0700

    fix(setup): add typescript and framer motion

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className='container flex-1 justify-center items-center '>
+      <h1 className='font-bold text-center text-5xl'>HELLO, COMING SOON.</h1>
+    </div>
+  )
+}
+
+export default Home
